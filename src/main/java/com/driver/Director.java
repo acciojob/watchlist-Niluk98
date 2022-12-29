5a10f0e134b3c972014b06e4d77b52ff0e342e41
niladri <niladriparamanik@gmail.com>
2022-12-29 16:50:03 +0530

    second

--- src/main/java/com/driver/Director.java
+++ src/main/java/com/driver/Director.java
@@ -13,4 +13,28 @@ public class Director {
         this.numberOfMovies = numberOfMovies;
         this.imdbRating = imdbRating;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfMovies() {
+        return numberOfMovies;
+    }
+
+    public void setNumberOfMovies(int numberOfMovies) {
+        this.numberOfMovies = numberOfMovies;
+    }
+
+    public double getImdbRating() {
+        return imdbRating;
+    }
+
+    public void setImdbRating(double imdbRating) {
+        this.imdbRating = imdbRating;
+    }
 }
